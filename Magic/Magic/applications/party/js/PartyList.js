149121ac539cb69a22e97bfdf897b64cf6a9171f
lchuang819 <316913962@qq.com>
2013-11-05 20:41:01 +0800

    party update

--- Magic/Magic/applications/party/js/PartyList.js
+++ Magic/Magic/applications/party/js/PartyList.js
@@ -1,7 +1,118 @@
-var PartyList = Ext.extend(Ext.app.Module,{
-	init:function(){
-		util.constant.debugEnable = true;
-		util.constant.errorEnable = true;
-		Ext.log('Open Console!');
+var partyList = Ext.extend(Ext.app.Module, {
+	moduleType : '',
+	moduleId : 'partyList',
+	text : '成员列表',
+	iconCls : 'icon-plugin',
+	init : function() {
+		this.createPanel();
+	},
+	createPanel : function() {
+
+		var menuTree = new Ext.tree.TreePanel({
+			region : 'west',
+			collapsible : true,
+			title : this.text,
+			width : 200,
+			height : this.height,
+			autoScroll : true,
+			autoDestroy:true,
+			closable : true,
+			split : true,
+			loader : new Ext.tree.TreeLoader(),
+			root : new Ext.tree.AsyncTreeNode({
+				expanded : true,
+				children : [ {
+					id : 'employeeDetails',
+					text : '雇员信息',
+					leaf : true,
+					iconCls:'icon-grid',
+					path:'applications/party/widget/newPartyGroup.ftl'
+				}, {
+					id : 'customerDetails',
+					text : '客户信息',
+					leaf : true,
+					iconCls:'icon-grid'
+				}, {
+					id : 'supplierDetails',
+					text : '供应商信息',
+					leaf : true,
+					iconCls:'icon-grid'
+				}]
+			}),
+			rootVisible : false,
+			
+			listeners : {
+				scope:this,
+				click : function(n) {
+					Ext.log('[PartyList :createPanel :listeners : click] is leaf=' + n.isLeaf());
+					if(n.isLeaf()){
+						this.openItem(n, n.attributes.path);
+					}else{
+						 if(n.expanded){
+							 n.collapse();
+						 }else{
+							 n.expand();
+						 }
+					}
+				}
+			}
+		});
+		
+		this.centerPanel = new Ext.TabPanel({
+			region : 'center',
+			title:'中心区'
+		});
+		
+		// Create main pannel
+		var ofbiz = new Ext.Panel({
+			id : 'partyListMainPanel',
+			title : this.text,
+			closable : true,
+			iconCls : this.iconCls,
+			layout : 'border',
+			items : [ menuTree, this.centerPanel ]
+		});
+		
+		desk.add(ofbiz);
+		desk.activate(ofbiz);
+
+	},
+	openItem : function (node, path) {
+		var id = node.id;
+		Ext.log('[PartyList :openItem] id=' + id);
+		if(id == 'employeeDetails'){
+			
+			Ext.log('[PartyList:openItem] init employeeDetails');
+			
+			var employeeDetailsPanel = new Ext.Panel({
+				id : 'employeeDetails',
+				title : '雇员列表',
+				frame : true,
+				closable : true,
+				autoDestroy : true,
+				autoScroll : true,
+				iconCls : 'icon-static',
+				autoLoad : {
+					url : util.constant.appPath + '/applications/party/widget/partyList.ftl', 
+					params : {roleTypeId : 'EMPLOYEE'},
+					scope : this, 
+					scripts : true,
+        			callback:function(el, success, response, options){
+        				Ext.log('[PartyList :openItem :callback ] success=' + success);
+    					if(!success){
+    						Ext.MessageBox.show({
+    							title:'错误',
+    							msg: '产品信息加载失败',
+    							buttons: Ext.Msg.OK,
+    							icon: Ext.MessageBox.ERROR
+    						});
+    					}
+    				}
+    			}
+			});
+			
+			this.centerPanel.add(employeeDetailsPanel);
+			this.centerPanel.activate(employeeDetailsPanel);
+		}
 	}
 });
--- Magic/Magic/applications/party/js/partyCreate.js
+++ Magic/Magic/applications/party/js/partyCreate.js
@@ -1,11 +1,10 @@
-var partyCreate=Ext.extend(Ext.app.Module,{
-    moduleType:'',
-    moduleId:'partyCreate',
-    text:'创建新成员或组织',
-    iconCls:'icon-plugin',
-	createWindow:function(){
-		
-		
+var partyCreate = Ext.extend(Ext.app.Module, {
+	moduleType : '',
+	moduleId : 'partyCreate',
+	text : '创建新成员或组织',
+	iconCls : 'icon-plugin',
+	createWindow : function() {
+
 		var menuTree = new Ext.tree.TreePanel({
 			region : 'west',
 			collapsible : true,
@@ -13,181 +12,124 @@ var partyCreate=Ext.extend(Ext.app.Module,{
 			width : 200,
 			height : this.height,
 			autoScroll : true,
-			autoDestroy:true,
+			autoDestroy : true,
 			closable : true,
 			split : true,
 			loader : new Ext.tree.TreeLoader(),
 			root : new Ext.tree.AsyncTreeNode({
 				expanded : true,
 				children : [ {
-					id : 'newPartyGroup',
-					text : '新建成员组',
+					id : 'SUPPLIER',
+					text : '创建供应商',
 					leaf : true,
-					iconCls:'',
-					path:'applications/party/widget/newPartyGroup.ftl'
+					iconCls : 'icon-user_biz'
 				}, {
-					id : 'NewCustomer',
+					id : 'CUSTOMER',
 					text : '创建客户',
 					leaf : true,
-					iconCls:''
+					iconCls : 'icon-user-gray'
 				}, {
-					id : 'NewPerson',
-					text : '创建人员',
+					id : 'EMPLOYEE',
+					text : '创建雇员',
 					leaf : true,
-					iconCls:''
-				}]
+					iconCls : 'icon-user-add'
+				} ]
 			}),
 			rootVisible : false,
-			
+
 			listeners : {
-				scope:this,
+				scope : this,
 				click : function(n) {
-					if(n.isLeaf()){
+					if (n.isLeaf()) {
 						this.openItem(n, n.attributes.path);
-					}else{
-						 if(n.expanded){
-							 n.collapse();
-						 }else{
-							 n.expand();
-						 }
+					} else {
+						if (n.expanded) {
+							n.collapse();
+						} else {
+							n.expand();
+						}
 					}
 				}
 			}
 		});
-		
+
 		this.centerPanel = new Ext.TabPanel({
-			
+
 			region : 'center',
-			title:'中心区'
+			title : '中心区'
 		});
-		
+
 		var ofbiz = new Ext.Panel({
-			id:this.moduleId,
-			title:this.text,
-			closable:true,
-			iconCls:this.iconCls,
+			id : this.moduleId,
+			title : this.text,
+			closable : true,
+			iconCls : this.iconCls,
 			layout : 'border',
-			items:[
-			       menuTree,
-			       this.centerPanel
-			]
+			items : [ menuTree, this.centerPanel ]
 		});
-		
-		//ofbiz.add(form);
+
+		// ofbiz.add(form);
 		desk.add(ofbiz);
 		desk.activate(ofbiz);
 	},
-	openItem:function(node, path){
+	openItem : function(node, path) {
+
+		var ssn = true;
+		var gener = true;
 		
-		if(path != null && path != ''){
-			var tab = new Ext.Panel({
-				id:node.id,
-				title:node.text,
-				autoScroll:true,
-				closable:true,
-				autoLoad:{url:util.constant.appPath+path,scripts:true}
-			});
-			
-			this.centerPanel.add(tab);
-			this.centerPanel.activate(tab);
-			
-		}else if(node.id != null && node.id != ''){
-			
-			var newcustomerPanel;
-			if(node.id == 'NewPerson'){
-				newcustomerPanel = new PartyForm({
-					actionType:node.id, 
-					gender:true, 
-					socialSecurityNumber:true, 
-					id:node.id, 
-					title:node.text,
-					loadParty:false,
-					buttons:[{
-			            text: '保存',
-			            type:'submit',
-			            handler:function(){
-						newcustomerPanel.form.submit({
-								url:'partyManager.action?actionType=NewPerson',
-				    		    method : 'POST',
-				    		    waitMsg : "正在创建……",
-				        		success:function(form,action){
-				        			alert('成功');
-				        		},
-				        		failure:function(form,action){
-				        			var errorMsg;
-			        				if(action.failureType=='connect'){
-			        					errorMsg = '连接服务器失败，请稍后再试';
-			        				}else if(action.failureType=='client'){
-			        					errorMsg = '输入信息有误';
-			        				}else{
-			        					errorMsg = action.result.msg;
-			        				}
-			        				Ext.log(errorMsg);
-			        				Ext.MessageBox.show({
-			        					title: '错误',
-			        					msg: errorMsg,
-			        					buttons: Ext.MessageBox.OK,
-			        					//fn: showResult,
-			        					icon:Ext.MessageBox.ERROR
-			        				});
-				        		}
-				        	});
-			        	}
-						},{
-				            text: '重置',
-				            type:'reset',
-				            handler:function(){newcustomerPanel.form.reset();}
-				        }]
-					});
-			}else{
-				newcustomerPanel = new PartyForm({
-					actionType:node.id, 
-					gender:false, 
-					socialSecurityNumber:false, 
-					id:node.id, 
-					title:node.text,
-					loadParty:false,
-					buttons:[{
-			            text: '保存',
-			            type:'submit',
-			            handler:function(){
-						newcustomerPanel.form.submit({
-								url:'partyManager.action?actionType=NewCustomer',
-				    		    method : 'POST',
-				    		    waitMsg : "正在创建……",
-				        		success:function(form,action){
-				        			alert('成功');
-				        		},
-				        		failure:function(form,action){
-				        			var errorMsg;
-				        				if(action.failureType=='connect'){
-				        					errorMsg = '连接服务器失败，请稍后再试';
-				        				}else if(action.failureType=='client'){
-				        					errorMsg = '输入信息有误';
-				        				}else{
-				        					errorMsg = action.result.msg;
-				        				}
-				        				Ext.MessageBox.show({
-				        					title: '错误',
-				        					msg: errorMsg,
-				        					buttons: Ext.MessageBox.OK,
-				        					//fn: showResult,
-				        					icon:Ext.MessageBox.ERROR
-				        				});
-				        		}
-				        	});
-			        	}
-						},{
-				            text: '重置',
-				            type:'reset',
-				            handler:function(){newcustomerPanel.form.reset();}
-				        }]
-					});
-			}
-		    
-			this.centerPanel.add(newcustomerPanel);
-			this.centerPanel.activate(newcustomerPanel);
+		if (node.id == 'SUPPLIER') {
+			ssn = false;
+			gener = false;
 		}
+
+		var newcustomerPanel = new PartyForm({
+			actionType : node.id,
+			gender : gener,
+			socialSecurityNumber : ssn,
+			id : node.id,
+			title : node.text,
+			loadParty : false,
+			buttons : [ {
+				text : '保存',
+				type : 'submit',
+				handler : function() {
+					newcustomerPanel.form.submit({
+						url : 'partyManager.action?actionType=NewCustomer',
+						method : 'POST',
+						waitMsg : "正在创建……",
+						success : function(form, action) {
+							alert('成功');
+						},
+						failure : function(form, action) {
+							var errorMsg;
+							if (action.failureType == 'connect') {
+								errorMsg = '连接服务器失败，请稍后再试';
+							} else if (action.failureType == 'client') {
+								errorMsg = '输入信息有误';
+							} else {
+								errorMsg = action.result.msg;
+							}
+							Ext.MessageBox.show({
+								title : '错误',
+								msg : errorMsg,
+								buttons : Ext.MessageBox.OK,
+								// fn: showResult,
+								icon : Ext.MessageBox.ERROR
+							});
+						}
+					});
+				}
+			}, {
+				text : '重置',
+				type : 'reset',
+				handler : function() {
+					newcustomerPanel.form.reset();
+				}
+			} ]
+		});
+
+		this.centerPanel.add(newcustomerPanel);
+		this.centerPanel.activate(newcustomerPanel);
 
 	}
 });
