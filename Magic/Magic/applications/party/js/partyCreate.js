2d4d40cafd9c8229c4167fdd34d5befc3c5a1080
lchuang819 <316913962@qq.com>
2013-11-17 17:04:50 +0800

    add

--- Magic/Magic/applications/party/js/partyCreate.js
+++ Magic/Magic/applications/party/js/partyCreate.js
@@ -19,20 +19,35 @@ var partyCreate = Ext.extend(Ext.app.Module, {
 			root : new Ext.tree.AsyncTreeNode({
 				expanded : true,
 				children : [ {
-					id : 'SUPPLIER',
-					text : '创建供应商',
+					id : 'SUPPLIER_CORPORATION',
+					text : '创建供应商(企业)',
 					leaf : true,
-					iconCls : 'icon-user_biz'
+					iconCls : 'icon-user_biz',
+					partyType : 'CORPORATION'
 				}, {
-					id : 'CUSTOMER',
-					text : '创建客户',
+					id : 'SUPPLIER_PERSON',
+					text : '创建供应商(个人)',
 					leaf : true,
-					iconCls : 'icon-user-gray'
+					iconCls : 'icon-user-gray',
+					partyType : 'PERSON'
+				}, {
+					id : 'CUSTOMER_CORPORATION',
+					text : '创建客户(企业)',
+					leaf : true,
+					iconCls : 'icon-user_biz',
+					partyType : 'CORPORATION'
+				}, {
+					id : 'CUSTOMER_PERSON',
+					text : '创建客户(个人)',
+					leaf : true,
+					iconCls : 'icon-user-gray',
+					partyType : 'PERSON'
 				}, {
 					id : 'EMPLOYEE',
 					text : '创建雇员',
 					leaf : true,
-					iconCls : 'icon-user-add'
+					iconCls : 'icon-user-add',
+					partyType : 'PERSON'
 				} ]
 			}),
 			rootVisible : false,
@@ -73,19 +88,10 @@ var partyCreate = Ext.extend(Ext.app.Module, {
 		desk.activate(ofbiz);
 	},
 	openItem : function(node, path) {
-
-		var ssn = true;
-		var gener = true;
-		
-		if (node.id == 'SUPPLIER') {
-			ssn = false;
-			gener = false;
-		}
-
+		Ext.log('Create Party and Type is:' + node.attributes.partyType);
 		var newcustomerPanel = new PartyForm({
 			actionType : node.id,
-			gender : gener,
-			socialSecurityNumber : ssn,
+			partyType : node.attributes.partyType,
 			id : node.id,
 			title : node.text,
 			loadParty : false,
@@ -94,18 +100,30 @@ var partyCreate = Ext.extend(Ext.app.Module, {
 				type : 'submit',
 				handler : function() {
 					newcustomerPanel.form.submit({
-						url : 'partyManager.action?actionType=NewCustomer',
+						url : 'partyManager.action',
 						method : 'POST',
 						waitMsg : "正在创建……",
+						params : {
+							actionType : 'NewPerson',
+							partyType : 'PERSON'
+						},
 						success : function(form, action) {
-							alert('成功');
+							Ext.log('==============successfully===============');
+							var s = Ext.util.JSON.encode(action.result.ResponseBody);
+							var partyId = action.result.ResponseBody.party.partyId;
+							var c = newcustomerPanel.find('id', 'partyId');
+							if(c.length > 0 && c[0]){
+								c[0].setText(partyId);
+							}
+							
+							Ext.MessageBox.alert('提示', '创建['+partyId+'],操作成功!');
 						},
 						failure : function(form, action) {
 							var errorMsg;
 							if (action.failureType == 'connect') {
-								errorMsg = '连接服务器失败，请稍后再试';
+								errorMsg = '连接服务器失败，请稍后再试!';
 							} else if (action.failureType == 'client') {
-								errorMsg = '输入信息有误';
+								errorMsg = '输入信息有误!';
 							} else {
 								errorMsg = action.result.msg;
 							}
@@ -130,6 +148,6 @@ var partyCreate = Ext.extend(Ext.app.Module, {
 
 		this.centerPanel.add(newcustomerPanel);
 		this.centerPanel.activate(newcustomerPanel);
-
+		
 	}
 });
